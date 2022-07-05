2586d9520601b84d4852e331702db1f6afea3f49
ThalizFajardo <fajardothaliz@gmail.com>
2022-07-05 00:44:50 -0500

    corregido

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,53 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 
-
 function App() {
-  const [data, setData] = useState({});
-  const [temp, setTemp] = useState(true)
-  const [units, setUnits] = useState("°F")
-
   //obtener fecha y hora 
   let date = new Date();
   let output = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear();
   let hora = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
   ////////////////////////////////////////////////////////
+  
+  const [data, setData] = useState({});
+  const [temp, setTemp] = useState(true)
+  const [units, setUnits] = useState("°F")
+  
+  
 
-/////////////////////////////////////////convertir unidads de temperatura//////////////////////////////
-  const Convert = () => {
+//////////////////////
+
+///////////////////convertir unidads de temperatura//////////////////////////////
+ const Convert = () => {
     if (units === "°F") {
-      setTemp((temp - 32) / 1.8)
+      setTemp((temp -32)*0.55)
       setUnits("°C")
     } else {
-      setTemp((temp * 1.8) + 32)
+      setTemp((temp*1.8)+32)
       setUnits("°F")
     }
   }
-
-  //peticion a http con use effect
-  useEffect(() => {
-    const success = pos => {
-      const lat = pos.coords.latitude;
-      const lon = pos.coords.longitude;
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=99c5b4521134966e5c2d3bf620bdd0ba`)
-
-        .then(res => {
+ 
+ //peticion a http con use effect
+ useEffect(() => {
+   const success = pos => {
+     const lat = pos.coords.latitude;
+     const lon = pos.coords.longitude;
+     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=99c5b4521134966e5c2d3bf620bdd0ba`)
+     
+     .then(res => {
           setData(res.data)
-          setTemp(res.data.main?.temp)
-
+          setTemp(1.8*(((res.data.main?.temp)-273)+32))
+          
         })
-    }
-    navigator.geolocation.getCurrentPosition(success);
-  }, [])
-
-
-  console.log(data)
-  ///////////////////////////////////////////////////////////////////////////////////////////  
-  return (
-    <div className="app">
+      }
+      navigator.geolocation.getCurrentPosition(success);
+    }, [])
+    
+    
+    console.log(data)
+    ///////////////////////////////////////////////////////////////////////////////////////////  
+    return (
+      <div className="app">
       <div className="card">
         <h1>Weather App</h1>
         <h2>{data.name} , {data.sys?.country}</h2>
@@ -56,7 +59,8 @@ function App() {
         <p><b>Pressure:</b>{data.main?.pressure} hPa</p>
         <p className="humidity"><b>Humidity:</b> {data.main?.humidity} %</p>
         <p className="wind"><b>Wind speed: </b>{data.wind?.speed} m/s</p>
-        <p ><b>Temp:</b>{temp} {units}</p>
+        <p ><b>Temp:</b>{temp.toFixed(2)} {units}</p>
+       
         <button onClick={Convert} >degrees °F / °C</button>
       </div>
     </div>
